6a7ee177ba1aebe53fabc3d1413525554634e100
Jude Bayham <judebayham@judes-mbp-2.lan>
2023-02-17 10:53:45 -0700

    minor updates to ts_prep

--- materials/unit_01/code/ts_prep.R
+++ materials/unit_01/code/ts_prep.R
@@ -5,17 +5,24 @@ library(dplyr)
 library(lubridate)
 
 ###
-egg_raw <- read_csv("materials/unit_01/inputs/WPU017107.csv")
+egg_raw <- read_csv("materials/unit_01/inputs/APU0000708111.csv")
 
 #Change format of date
 egg_raw %>%
   mutate(CHAR_DATE=format(DATE,"%m/%d/%Y")) %>%
   select(-DATE) %>%
-  write_csv("materials/unit_01/inputs/WPU017107_cdate.csv")
+  write_csv("materials/unit_01/inputs/APU0000708111_cdate.csv")
+
+
+egg_raw_c <- read_csv("https://csu-arec-330.github.io/materials/unit_01/inputs/APU0000708111_cdate.csv")
+
 
 library(tidyquant)
 
 
-tq_get(c("WPU017107","WPU012"),get = "economic.data",from="1990-01-01")
+tq_get(c("APU0000708111"),get = "economic.data",from="1990-01-01")
 
-tq_get(c("WPU017107","WPU012"),get = "economic.data",from="1990-01-01")
+#WPU017107: Producer Price Index by Commodity: Farm Products: Eggs for Fresh Use
+#WPU012: Producer Price Index by Commodity: Farm Products: Grains
+#WPU023: Producer Price Index by Commodity: Processed Foods and Feeds: Dairy Products 
+fred_data <- tq_get(c("WPU017107","WPU012","WPU023"),get = "economic.data",from="1900-01-01")
