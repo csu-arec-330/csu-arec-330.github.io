0b4c1f4ac7612f7cf4198f8eb37867388df5129c
Jude Bayham <judebayham@judes-mbp-2.lan>
2023-02-24 12:19:27 -0700

    adding forecast part to unit 1 week 2

--- materials/unit_01/code/ts_prep.R
+++ materials/unit_01/code/ts_prep.R
@@ -26,3 +26,101 @@ tq_get(c("APU0000708111"),get = "economic.data",from="1990-01-01")
 #WPU012: Producer Price Index by Commodity: Farm Products: Grains
 #WPU023: Producer Price Index by Commodity: Processed Foods and Feeds: Dairy Products 
 fred_data <- tq_get(c("WPU017107","WPU012","WPU023"),get = "economic.data",from="1900-01-01")
+
+
+#######################################
+#Lab 02
+
+#Tutorial here: https://kevin-kotze.gitlab.io/tsm/ts-2-tut/
+install.packages("changepoint")
+library(changepoint)
+install.packages("strucchange")
+library(strucchange)
+set.seed(123) 
+
+sim_mean <- c(rnorm(180, 0, 1),
+              rnorm(180, 1.5, 1))
+
+sim_mean <- rnorm(360, 0, 1)
+              
+plot.ts(sim_mean)
+
+#m_binseg <- cpt.mean(sim_mean, penalty = "BIC", method = "BinSeg", Q = 5)
+
+#plot(m_binseg, type = "l", xlab = "Index", cpt.width = 4)
+
+reg_dat <- tibble(y=sim_mean) %>%
+  mutate(x=row_number())
+                  
+test_lm <- lm(y ~ x,data=reg_dat)
+summary(test_lm)
+
+#Testing for structural breaks
+fs <- Fstats(y ~ x,data=reg_dat,from = 100,to=200)
+summary(fs)
+plot(fs)
+plot(fs,pval=T)
+
+#Recreate Fstats output to understand
+cut1=100
+reg_dat1 <- reg_dat[1:cut1,]
+reg_dat2 <- reg_dat[cut1+1:nrow(reg_dat),]
+
+m1 <- lm(y ~ x,data = reg_dat1)
+m2 <- lm(y ~ x,data = reg_dat2)
+
+m1$residuals
+
+################################
+#Read in Ali's carrots data
+carrot <- tq_get(c("WPU01130212"),get = "economic.data",from="2007-08-01")
+
+carrot_ts <- ts(carrot$price,frequency = 12,start=c(2007,8))
+
+carrot_decomp <- decompose(carrot_ts)
+
+plot(carrot_decomp$trend)
+
+
+plot(carrot_decomp)
+
+carrot_decomp_out <- carrot_decomp[1:4] %>%
+  as_tibble() %>%
+  rename(price=x) %>%
+  mutate(measure_date=carrot$date)
+
+write_csv(carrot_decomp_out,"carrot_decomp.csv")
+
+# Extract the trend, seasonal, and residual components
+carrot_trend <- carrot_decomp$trend
+carrot_seasonal <- carrot_decomp$seasonal
+carrot_residuals <- carrot_decomp$random
+
+library(forecast)
+
+# Forecast the trend component for the next 12 months
+carrot_trend_forecast <- forecast(carrot_trend, h=60)
+
+plot(carrot_trend_forecast)
+
+# Forecast the seasonal component for the next 12 months
+carrot_seasonal_forecast <- forecast(carrot_seasonal, h=60)
+
+plot(carrot_seasonal_forecast)
+
+# Create a time series object for the residuals
+carrot_residuals_ts <- ts(carrot_residuals, frequency=12, start=c(2007, 8))
+
+# Fit an MA(1) model to the residuals
+carrot_residuals_model <- arima(carrot_residuals_ts, order=c(0,0,1))
+
+# Forecast the residuals for the next 12 months
+carrot_residuals_forecast <- forecast(carrot_residuals_model, h=60)$mean
+
+# Combine the forecasted components to obtain the final forecast
+carrot_forecast <- carrot_trend_forecast$mean + carrot_seasonal_forecast$mean + carrot_residuals_forecast
+
+# Print the forecast for the next 12 months
+plot(carrot_forecast)
+
+
