1fcddeb67a8610daa48c177176cc92fc9aaf6fa1
Lauren Chenarides <Lauren.Chenarides@colostate.edu>
2025-02-25 15:33:18 -0700

    updates to ps6

---
+++ materials/unit_01/week_02/lab_01_week_02_solution.R
@@ -0,0 +1,76 @@
+# This is the script for lab 05.
+
+install.packages("forecast")
+
+# Load necessary libraries
+library(tidyquant)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(forecast)
+
+# Retrieve time series data for carrots
+carrot <- tq_get(c("WPU01130212"),
+                 get = "economic.data",
+                 from = "2007-08-01")
+
+# Convert data into a time series object
+carrot_ts <- ts(carrot$price,
+                frequency = 12,
+                start = c(2007, 8))
+
+# Decompose the time series
+carrot_decomp <- decompose(carrot_ts)
+
+# Plot decomposed components
+plot(carrot_decomp$trend)
+plot(carrot_decomp$seasonal)
+plot(carrot_decomp$random)
+
+# Store decomposition results into a dataframe
+carrot_decomp_out <- carrot_decomp[1:4] %>%
+  as_tibble() %>%
+  rename(price = x) %>%
+  mutate(measure_date = carrot$date,
+         forecast = FALSE) %>%
+  drop_na()
+
+# Extract trend, seasonal, and residual components
+carrot_trend <- na.omit(carrot_decomp$trend)
+carrot_seasonal <- na.omit(carrot_decomp$seasonal)[7:(length(carrot_ts) - 6)] %>%
+  ts(., start = c(2008, 2), frequency = 12)
+carrot_residuals <- na.omit(carrot_decomp$random)
+
+# Forecast trend and seasonal components
+carrot_trend_forecast <- forecast(carrot_trend, level = 95, h = 60)
+plot(carrot_trend_forecast)
+
+carrot_seasonal_forecast <- forecast(carrot_seasonal, level = 95, h = 60)
+plot(carrot_seasonal_forecast)
+
+# Fit an MA(1) model for residuals and forecast
+carrot_residuals_ts <- ts(carrot_residuals, frequency = 12, start = c(2007, 8))
+carrot_residuals_model <- arima(carrot_residuals_ts, order = c(0,0,1))
+carrot_residuals_forecast <- forecast(carrot_residuals_model, level = 95, h = 60)
+
+# Combine forecasted components
+carrot_forecast <- carrot_trend_forecast$mean + carrot_seasonal_forecast$mean + carrot_residuals_forecast$mean
+
+carrot_forecast_upper <- carrot_trend_forecast$upper + carrot_seasonal_forecast$upper + carrot_residuals_forecast$upper
+carrot_forecast_lower <- carrot_trend_forecast$lower + carrot_seasonal_forecast$lower + carrot_residuals_forecast$lower
+
+# Convert forecast into a dataframe
+carrot_forecast_df <- tibble(price = carrot_forecast,
+                             upper = carrot_forecast_upper[,1],
+                             lower = carrot_forecast_lower[,1]) %>%
+  mutate(measure_date = seq(as_date("2023-08-01"), by = "months", length.out = nrow(.)))
+
+# Append forecasted data to the existing decomposition dataset
+carrot_forecast_df <- carrot_forecast_df %>%
+  filter(measure_date > max(carrot_decomp_out$measure_date)) %>%
+  mutate(forecast = TRUE)
+
+final_out <- bind_rows(carrot_decomp_out, carrot_forecast_df)
+
+# Export data for Tableau
+write_csv(final_out, "carrot_forecast.csv")
